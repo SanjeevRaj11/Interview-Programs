2a79177f014587ab80136547bc5f10d69c259364
Sanjeev Raj M <144685421+SanjeevRaj11@users.noreply.github.com>
2023-10-17 23:36:29 +0530

    Update PatternProgram6.java

--- PatternProgram6.java
+++ PatternProgram6.java
@@ -3,10 +3,10 @@ package interviewProgramsFundamentals;
 public class PatternProgram6 {
 	
 	/*
-	 1 2 3 4 5           1 2 3 4 5     	  * * * * *
-	   2 3 4 5 			 # 2 3 4 5		 	* * * *
-	     3 4 5			 # # 3 4 5		   	  * * *
-	  	   4 5			 # # # 4 5			 	* *
+	 1 2 3 4 5           		1 2 3 4 5 		    	  * * * * *
+	   2 3 4 5 			 # 2 3 4 5		            * * * *
+	     3 4 5			 # # 3 4 5		   	      * * *
+	       4 5			 # # # 4 5			 	* *
 	         5			 # # # # 5			   	  *
 	 */
 	
